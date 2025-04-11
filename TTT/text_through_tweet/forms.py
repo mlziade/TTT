969d3edbf34da3a36e5ddb5e050f4fae03a387bb
mlziade <ziade.michel@hotmail.com>
2025-04-11 02:20:02 -0300

    Refactor forms.py to ensure proper formatting and maintain consistency in form definitions

--- TTT/text_through_tweet/forms.py
+++ TTT/text_through_tweet/forms.py
@@ -2,3 +2,7 @@ from django import forms
 
 class TextForm(forms.Form):
     text = forms.CharField(label="Text", max_length=280)
+
+class CreateTextThroughTweetForm(forms.Form):
+    text = forms.CharField(label="Text", max_length=280)
+    ciphered_text = forms.CharField(label="Ciphered Text", max_length=280, required=False)
